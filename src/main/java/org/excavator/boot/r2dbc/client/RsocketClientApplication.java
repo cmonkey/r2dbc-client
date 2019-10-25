83706f200488dcd7c37ed208498959723b78fc17
CMonkey <42.codemonkey@gmail.com>
2019-10-25 11:00:00 +0800

    add SpringBootApplication Annotation to RsocketClientApplication

--- src/main/java/org/excavator/boot/r2dbc/client/RsocketClientApplication.java
+++ src/main/java/org/excavator/boot/r2dbc/client/RsocketClientApplication.java
@@ -2,8 +2,10 @@ package org.excavator.boot.r2dbc.client;
 
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.rsocket.RSocketRequester;
+@SpringBootApplication
 public class RsocketClientApplication{
 
     @SneakyThrows
